64759aec8f494cd0862524c5329b7a8abbc33fa0
Erwin Darsono <erwindarsono08@gmail.com>
2023-06-25 20:13:40 +0700

    add fonts and making login page

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path'
 
+//instantiate express
 const app = express();
+//to use dotenv variables
 dotenv.config();
 
+//resolving staticpath
 const staticPath = path.resolve('public');
 app.use(express.static(staticPath));
 
+//template engine = .ejs
+app.set("view engine", "ejs");
 
+//routes
+app.get('/', (req,res) => {
+    res.render('login');
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
