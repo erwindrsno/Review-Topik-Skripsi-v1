4fce80be1bc5614c761b8e84476f5c7cb26f0d6c
Erwin Darsono <erwindarsono08@gmail.com>
2023-07-11 23:45:25 +0700

    remove unused try catch

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,46 +8,37 @@ export const register = async (req, res) => {
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        try {
-            const [user, created] = await User.findOrCreate({
-                where: { academic_num: req.body.academic_num },
-                defaults: {
-                    name: req.body.name,
-                    username: req.body.username,
-                    password: hashedPassword,
-                    academic_num: req.body.academic_num,
-                    specialization: req.body.specialization
-                }
-            });
-
-            if (created) {
-                const role = await Role.findOne({ where: { role_name: 'mahasiswa' } })
-                if (role === null) {
-                    res.status(500).send("Role tidak ditemukan");
-                }
-                else {
-                    try {
-                        await user.addRole(role);
-                        console.log('Role associated: ' + role.role_name);
-                    }
-                    catch (err) {
-                        console.log(err);
-                    }
-                }
+        const [user, created] = await User.findOrCreate({
+            where: { academic_num: req.body.academic_num },
+            defaults: {
+                name: req.body.name,
+                username: req.body.username,
+                password: hashedPassword,
+                academic_num: req.body.academic_num,
+                specialization: req.body.specialization
             }
-        } catch (err) {
-            console.log(err);
-            res.status(500).send();
+        });
+
+        if (created) {
+            const role = await Role.findOne({ where: { role_name: 'mahasiswa' } })
+            if (role === null) {
+                res.status(500).send("Role tidak ditemukan");
+            }
+            else {
+                await user.addRole(role);
+                console.log('Role associated: ' + role.role_name);
+            }
         }
         res.status(200).send();
-
-    } catch {
+    }
+    catch (err){
+        console.log(err)
         res.status(500).send();
     }
 }
 
 export const login = async (req, res) => {
-    //harusnya validation dulu di frontend
+    //harusnya validation dulu di frontend (min 8 char, harus ada special char, nomor dan capital);
     const loginUser = await User.findOne({ where: { username: req.body.username } });
     if (loginUser === null) {
         console.log("Login failed");
