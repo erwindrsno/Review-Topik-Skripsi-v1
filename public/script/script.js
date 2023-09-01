271e24442f36c48aec2490744e1c366bee3c2388
Erwin Darsono <erwinschmidtzerz@gmail.com>
2023-09-01 12:54:27 +0700

    login/register tab done

--- public/script/script.js
+++ public/script/script.js
@@ -6,14 +6,18 @@ console.log("Script triggered!");
 
   const loginForm = document.getElementById('loginForm');
   loginForm.classList.add('active');
+  tabs[0].classList.add('selected');
 
   tabs.forEach(tab => {
     tab.addEventListener('click', function (event) {
       event.preventDefault();
 
-      if(document.querySelector('.active')){
-        tab.style.backgroundColor = '#001b2e';
-      }
+      // tab.classList.add('selected');
+      tabs.forEach(tab => {
+        tab.classList.remove('selected');
+      });
+
+      tab.classList.add('selected');
 
       const targetForm = this.getAttribute('data-form');
 
