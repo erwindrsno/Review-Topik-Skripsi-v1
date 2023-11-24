40ec2ad50b1012ad01248b1cf8dba6f45fe1a859
Erwin Darsono <erwindarsono08@gmail.com>
2023-11-24 12:58:27 +0800

    add period model

---
+++ models/Period.js
@@ -0,0 +1,18 @@
+import db from "../database/config.js";
+import { Sequelize, DataTypes } from 'sequelize';
+
+const Period = db.define('Period', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    semester: {
+        type: DataTypes.BOOLEAN,
+    },
+    year:{
+        type: DataTypes.STRING(9)
+    }
+});
+
+export default Period;
